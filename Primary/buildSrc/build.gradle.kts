302d0a45efa231914b8b5ac2d6912c66c6940d5a
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-02-11 10:16:51 -0800

    initial commit

---
+++ Primary/buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    // Use JCenter for resolving dependencies.
+    jcenter()
+}
+
+dependencies {
+    implementation("net.keturn.idea-261518:lib:+")
+}
